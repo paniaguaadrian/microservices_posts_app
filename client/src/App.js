32884abad06cf20001577c8a72262e136297d074
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-14 02:16:56 +0100

    Create post component created, also submitHandler.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import PostCreate from "./components/PostCreate";
+
 export default () => {
-  return <div>Blog app</div>;
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
