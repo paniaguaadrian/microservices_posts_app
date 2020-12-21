2f369090e0b21fcbbf06febbebff14452a1f0399
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2020-12-21 13:50:49 +0100

    Updated components to take data from query service.

--- client/src/components/Posts/PostList.jsx
+++ client/src/components/Posts/PostList.jsx
@@ -9,7 +9,8 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    // * We change the PORT of localhost from 4000 to 4002 bc we want to target the query service.
+    const res = await axios.get("http://localhost:4002/posts");
     setPosts(res.data);
   };
 
@@ -26,7 +27,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
